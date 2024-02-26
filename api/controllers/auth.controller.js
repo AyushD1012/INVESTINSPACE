cf2690adfab203d7b716b0df3400de581b3a5966
Ayush <shubhd1012@gmail.com>
2024-02-27 00:19:34 +0530

    sign api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+
+import jwt from "jsonwebtoken";
 
 export const signup=async(req,res,next) => {
     const {username ,email ,password}=req.body;
@@ -9,6 +12,32 @@ export const signup=async(req,res,next) => {
         await newUser.save()
         //saving a login creditial takes time so we want first one to completely save until then other wait
         res.status(201).json("User created successfully!");
+    }catch(error){
+        next(error);
+
+    }
+    
+}
+
+
+
+export const signin=async(req,res,next) => {
+    const {email,password}=req.body;
+   
+    try{
+        const validUser= await User.findOne({email});
+        if(!validUser) return next(errorHandler(404,'User not found'));
+        const validPassword= bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword) return next(errorHandler(401,'Wrong credentials'));
+
+        const token= jwt.sign({id:validUser._id}, process.env.JWT_SECRET);
+
+        //to show show password of user and rest here is all info without password that we want to show
+        const {password:pass, ...rest}=validUser._doc;
+
+        res.cookie('access_token',token,{httpOnly: true})
+        .status(200)
+        .json(rest);
     }catch(error){
         next(error);
 
