fd3b642834ec41f4e46248a53a3fa509aa0cbdcc
Ayush <shubhd1012@gmail.com>
2024-03-02 01:25:04 +0530

    profile picture input and updating wronging written avatar photo and learn about using outlet and navigate of resct router dom

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -21,7 +21,7 @@ const userSchema=new mongoose.Schema({
     },
     avatar:{
         type:String,
-        default:"https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.winhelponline.com%2Fblog%2Fwp-content%2Fuploads%2F2017%2F12%2Fuser.png&tbnid=_sT5BJJAq9Y_VM&vet=12ahUKEwi4t9Dd486EAxXwSGwGHTCoCu4QMygaegUIARCYAQ..i&imgrefurl=https%3A%2F%2Fwww.winhelponline.com%2Fblog%2Freplace-default-user-account-picture-avatar-windows-10%2F&docid=BlO5mdfz_MWV_M&w=256&h=256&q=default%20user%20photo&ved=2ahUKEwi4t9Dd486EAxXwSGwGHTCoCu4QMygaegUIARCYAQ"
+        default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTcoK-dlbjL7C1eAPifV1gUs2n6ukUugyM-J5wFbSEptPEn7GCoJXnFXMDlp9SdP-JcIQ&usqp=CAU"
     }
 }, {timestamps: true});
 
