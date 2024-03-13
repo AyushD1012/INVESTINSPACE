355ad5ad8d3f70ed6ec17c2e685b40f4969b8149
Ayush <shubhd1012@gmail.com>
2024-03-13 16:36:00 +0530

    delete lisiting button functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -142,6 +142,23 @@ export default function Profile() {
       setShowListingsError(true);
     }
   };
+
+  const handleListingDelete= async (listingId)=>{
+    try {
+      
+      const res = await fetch(`/api/listing/delete/${listingId}`,{
+        method:'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+      setUserListings((prev)=>prev.filter((listing)=> listing._id !== listingId));
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
   return (
     <div className="bg-white  rounded-lg max-w-3xl mx-auto">
       <h1 className="text-4xl font-semibold my-10 p-2  text-center ">
@@ -261,7 +278,7 @@ export default function Profile() {
               <p>{listing.name}</p>
             </Link>
             <div className="flex flex-col items-center">
-              <button className="text-red-700 uppercase">Delete</button>
+              <button onClick={()=>handleListingDelete(listing._id)} className="text-red-700 uppercase">Delete</button>
               <button className="text-green-700 uppercase">Edit</button>
             </div>
           </div>
